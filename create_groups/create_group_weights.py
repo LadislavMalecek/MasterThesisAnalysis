b93af97f51bed6098987ac0423ab363e99af0cc3
Ladislav Malecek <malecek.ladislav@gmail.com>
2022-12-22 23:25:18 +0100

    Creation of group weights added

---
+++ create_groups/create_group_weights.py
@@ -0,0 +1,30 @@
+import argparse
+import os
+import numpy as np
+
+
+def generate(group_size, num_groups, file_name):
+    rand = np.random.rand(num_groups, group_size)
+    array = np.divide(rand, rand.sum(axis=1).reshape((-1, 1)))
+    np.savetxt(file_name, array, delimiter=',', fmt='%.2f')
+
+
+if __name__ == "__main__":
+    def parse_args():
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--output-dir', help='The directory where the resulting matrices will be saved. Default is "groups" dir under the input data directory.')
+        parser.add_argument('--group-sizes', default='4,6,8', type=str, help='Sizes of groups, numbers divided by a comma. Ex.: 5,6,7,8')
+        parser.add_argument('--num-groups-to-generate', default=1000, type=int, help='Number of groups to generate.')
+
+        args = parser.parse_args()
+        args.group_sizes_list = list(map(lambda x: int(str.strip(x)), args.group_sizes.split(',')))
+        return args
+
+if __name__ == '__main__':
+    args = parse_args()
+    os.makedirs(args.output_dir, exist_ok=True)
+
+    for g_size in args.group_sizes_list:
+        filename = 'group_weights_' + str(g_size)
+        file = os.path.join(args.output_dir, filename)
+        generate(g_size, 1000, file)
--- run_experiments.sh
+++ run_experiments.sh
@@ -20,6 +20,8 @@ poetry run python gather_datasets/download_and_transform.py --datasets kgrec
 green "Running dataset downloader and transformer for spotify"
 poetry run python gather_datasets/download_and_transform.py --datasets spotify
 
+
+
 # Get the ground truths
 green "Running matrix factorization for kgrec"
 poetry run python ./matrix_factorization/matrix_factorization.py --rating-type implicit --input ./datasets/kgrec/music_ratings.csv.gz --num-factors 50 --num-iterations 15 --random-seed 42
@@ -48,4 +50,31 @@ do
   poetry run python ./create_groups/create_topk_groups.py --group-sizes 4,6,8 --num-groups-to-generate 1000 --num-of-candidates 1000 --input $dataset
   poetry run python ./create_groups/create_prs_groups.py --scaling-exponent 1 --group-sizes 4,6,8 --num-groups-to-generate 1000 --num-of-candidates 1000 --input $dataset
   poetry run python ./create_groups/create_prs_groups.py --scaling-exponent 4 --group-sizes 4,6,8 --num-groups-to-generate 1000 --num-of-candidates 1000 --input $dataset
+done
+
+
+
+datasets=(
+   ./datasets/kgrec
+   ./datasets/movie_lens_small
+   ./datasets/kgrec
+   ./datasets/netflix
+)
+
+
+# single run experiments
+# iterate through all files in the ./datasets/groups
+for dataset in "${datasets[@]}"
+do
+  # get all files in the directory $dataset/groups
+  echo 'Running group recommender for' $file
+  poetry run python ./experiments/run_algorithms.py --input-groups-directory $dataset/groups --input_mf $datasets/mf/
+done
+
+# create group weights for the weighted experiments
+for dataset in "${datasets[@]}"
+do
+  # get all files in the directory $dataset/groups
+  echo 'Creating group weights for' $dataset
+  poetry run python ./create_groups/create_group_weights.py --output-dir $dataset/groups/weights
 done
